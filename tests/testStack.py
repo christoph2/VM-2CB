7dc27b716e2437314e80776f31ef8b6701300387
Christoph Schueler <cpu12.gems@googlemail.com>
2013-01-09 12:48:39 +0100

    Added 'testStack.py'.

--- tests/testStack.py
+++ tests/testStack.py
@@ -2,21 +2,22 @@
 import unittest
 import vm_al as v
 
+class StackTest(unittest.TestCase):
+	def __init__(self, *args, **kwargs):
+		super(StackTest, self).__init__(*args, **kwargs)
+		self.arr = v.byteArray(64)
+		v.setRAMPointer(self.arr)
 
-values = (1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999)
-
-def main():
-  arr = v.byteArray(64)
-  v.setRAMPointer(arr)
-  for w in values:
-    v.VM_PushW(w)
-    
-  v.VM_Sub()
-  while v.getSP():
-    x = v.VM_PopW()
-    print x
-
+	def testOrder(self):
+		values = (1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999)
+		for w in values:
+    			v.VM_PushW(w)
+		result = []
+		while v.getSP():
+			result.append(v.VM_PopW())
+		result.reverse()
+		self.assertEquals(values, tuple(result))
   
 if __name__ == '__main__':
-  main()
   unittest.main()
+ 
