d7ea4881573f0ede80a31b340b09d635a3b24ff5
Christoph Schueler <cpu12.gems@googlemail.com>
2016-03-01 12:44:03 +0000

    Make C-extension more verbose.

--- test/src/examp.c
+++ test/src/examp.c
@@ -1,8 +1,10 @@
 #include <Python.h>
+#include <stdio.h>
 
 PyMODINIT_FUNC initexamp(void)
 {
     PyObject *m;
         
     m = Py_InitModule( "examp", NULL );
+    printf("Hello from C-extension.\n");
 } 
